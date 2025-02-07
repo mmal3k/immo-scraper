ded07027c8ade35db88641f69028effc8499f261
arslan <arslanlrb@gmail.com>
2025-02-07 22:32:34 +0100

    Added ville() function

--- main.py
+++ main.py
@@ -7,25 +7,13 @@ class NonValide(Exception) :
 def getsoup(url):
     page = requests.get(url).text
     soup = BeautifulSoup(page , "html.parser")
-    prix = soup.find("p" , class_="product-price")
-    if prix :
-        prix_text = prix.text.strip()  # Remove extra spaces
-        prix_text = prix_text.replace("€", "").strip()
-        prix_numeric = int("".join(prix_text.split()))
-        
-        if prix_numeric < 10000 : 
-            raise NonValide
-
-    
-        print(f"le prix : {prix_text}")  # Print cleaned price
-
-    # print(soup.prettify())
-
-def prix(soup) :
-
-    pass
-
-
-getsoup("https://www.immo-entre-particuliers.com/annonce-lot-et-garonne-miramont-de-guyenne/409264-garage-garde-meuble")
+    return soup 
+def ville(soup):
+    ville = soup.find("h2" , class_="mt-0")
+    index = ville.text.rfind(',')
+    print(ville.text[index+1:].strip())
+  
+soup = getsoup("https://www.immo-entre-particuliers.com/annonce-lot-et-garonne-miramont-de-guyenne/409264-garage-garde-meuble")
+ville(soup)
 
 
