dc6e7fe4a89a5a5bc0ddaf3b95299a59d5198d15
mmal3k <mmalekrais@gmail.com>
2025-02-07 23:03:59 +0100

    reached question 6

--- main.py
+++ main.py
@@ -7,7 +7,47 @@ class NonValide(Exception) :
 def getsoup(url):
     page = requests.get(url).text
     soup = BeautifulSoup(page , "html.parser")
-    prix(soup)
+    
+    print(type(soup))
+    print(surface(soup))
+    print(nbrpieces(soup))
+    print(nbrchambres(soup))
+    print(nbrsdb(soup))
+    print(dpe(soup))
+    
+
+
+def caract(soup , index) :
+    caract = soup.find("div" , class_="product-features")
+    caract = caract.find("ul").findAll("li")
+    res = []
+    for i in caract :
+        res.append(i.find("span" , class_ = "fw-bold").text.split("\n"))
+    
+    return res[index][0]
+
+
+def type(soup) :
+    return caract(soup , 0)
+
+def surface(soup) :
+    surface = caract(soup , 1).replace("m²" ,"")
+    return surface
+
+    
+def nbrpieces(soup) :
+    return caract(soup , 3)
+    
+
+def nbrchambres(soup) :
+    return caract(soup , 4)
+
+def nbrsdb(soup) :
+    return caract(soup , 5)
+
+def dpe(soup) :
+    dpe = caract(soup , 13)[0]
+    return dpe
 
 def prix(soup) :
     prix = soup.find("p" , class_="product-price")
